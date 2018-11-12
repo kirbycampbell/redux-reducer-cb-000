32cee82f9dfd7ebea1a686ff586c72a7aecf47ce
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-12 18:26:06 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -6,8 +6,13 @@ export function manageFriends(state = { friends: [],}, action){
       return Object.assign({}, state, { friends: [...state.friends, action.friend]});
 
       case "REMOVE_FRIEND":
-        return Object.assign({}, state, { friends: [...state.friends, action.friend]});
-
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends.slice(0, removalIndex),
+          ...state.friends.slice(removalIndex + 1)
+        ]
+      });
       default:
         return state;
   }
