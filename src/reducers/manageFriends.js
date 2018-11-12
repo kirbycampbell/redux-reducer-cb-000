8de8f4abfa39970ad054522ae7095afec5ee8ed1
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-12 18:20:11 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,5 +3,8 @@ export function manageFriends(state, action){
   switch (acount.type){
     case "ADD_FRIEND":
       return { count: state.count + 1}
+
+      default:
+        return state;
   }
 }
