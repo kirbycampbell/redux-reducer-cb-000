658ad083ba4d82e151f56f54c20279f61e744877
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-12 18:19:26 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,9 +1,9 @@
 export function managePresents(state, action){
   switch (action.type) {
     case "INCREASE":
-      return { numberOfPresents: state.count + 1}
+      return { number: state.numberOfPresents + 1}
     case "DECREASE":
-      return { numberOfPresents: state.count - 1}
+      return { count: state.numberOfPresents - 1}
       default:
         return state;
   }
